fc4c960602e4736d6def18827839c0c9cfdfe0de
Michael Geeraert <mgeeraert@vendasta.com>
2018-09-01 15:17:59 -0600

    add firebase and create firestore database service

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,30 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+
+import * as firebase from 'firebase';
+import { firestore } from 'firebase/app'
+import {FirestoreService} from "./firestore.service";
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit{
   title = 'wedd-site';
+  constructor(private firestoreService: FirestoreService) {}
+
+
+  ngOnInit(): void {
+    firebase.initializeApp({
+      apiKey: 'AIzaSyCBdWiutvUMnzw-uqEtRt1RsGrc9MPzqDk',
+      authDomain: 'wedd-site.firebaseapp.com',
+      databaseURL: 'https://wedd-site.firebaseio.com',
+      projectId: 'wedd-site',
+      storageBucket: 'wedd-site.appspot.com',
+      messagingSenderId: '469140619713'
+    });
+    const db = firebase.firestore();
+    this.firestoreService.initialize(db);
+
+  }
 }
