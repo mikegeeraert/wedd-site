4bb5e44ffc73e11baab426e4396fa58c610716c8
Michael Geeraert <mgeeraert@vendasta.com>
2018-09-02 11:40:07 -0600

    add material support, menu and sidenav

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,17 +1,26 @@
-import {Component, OnInit} from '@angular/core';
+import {ChangeDetectorRef, Component, OnDestroy, OnInit} from '@angular/core';
 
 import * as firebase from 'firebase';
 import { firestore } from 'firebase/app'
 import {FirestoreService} from "./firestore.service";
+import {MediaMatcher} from "@angular/cdk/layout";
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent implements OnInit{
+export class AppComponent implements OnInit, OnDestroy {
   title = 'wedd-site';
-  constructor(private firestoreService: FirestoreService) {}
+  mobileQuery: MediaQueryList;
+
+
+  constructor(private firestoreService: FirestoreService, changeDetectorRef: ChangeDetectorRef, media: MediaMatcher) {
+
+    this.mobileQuery = media.matchMedia('(max-width: 600px)');
+    this._mobileQueryListener = () => changeDetectorRef.detectChanges();
+    this.mobileQuery.addListener(this._mobileQueryListener);
+  }
 
 
   ngOnInit(): void {
@@ -27,4 +36,14 @@ export class AppComponent implements OnInit{
     this.firestoreService.initialize(db);
 
   }
+
+  fillerNav = Array.from({length: 50}, (_, i) => `Nav Item ${i + 1}`);
+
+  private _mobileQueryListener: () => void;
+
+  ngOnDestroy(): void {
+    this.mobileQuery.removeListener(this._mobileQueryListener);
+  }
+
+  shouldRun = [/(^|\.)plnkr\.co$/, /(^|\.)stackblitz\.io$/].some(h => h.test(window.location.host));
 }
