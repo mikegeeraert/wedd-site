38688b6570c2bad2b00d31c4de21ef2f1107c7d7
Michael <mikegeeraert@gmail.com>
2018-11-21 21:27:47 -0600

    build out the rsvp form

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,18 +2,26 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
-import {MatButtonModule, MatIconModule, MatListModule, MatToolbarModule} from '@angular/material';
+import {
+  MatButtonModule, MatCardModule, MatCheckboxModule, MatChipsModule, MatFormFieldModule, MatIconModule, MatInputModule, MatListModule,
+  MatStepperModule,
+  MatToolbarModule
+} from '@angular/material';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { MatSidenavModule } from '@angular/material/sidenav';
 
 
 import { RsvpFormComponent } from './rsvp-form/rsvp-form.component';
 import { OurStoryComponent } from './our-story/our-story.component';
-import {RouterModule, Routes} from '@angular/router';
+import { RouterModule, Routes} from '@angular/router';
+import {FormBuilder, FormsModule, ReactiveFormsModule} from '@angular/forms';
+import { ChipListComponent } from './rsvp-form/chip-list/chip-list.component';
 
 const routes: Routes = [
+  {path: '', component: OurStoryComponent},
   {path: 'rsvp', component: RsvpFormComponent},
-  {path: '**', component: OurStoryComponent},
+  {path: 'rsvp/:userId', component: RsvpFormComponent},
+  {path: '**', redirectTo: ''},
 ];
 
 @NgModule({
@@ -21,21 +29,36 @@ const routes: Routes = [
     AppComponent,
     RsvpFormComponent,
     OurStoryComponent,
+    ChipListComponent,
   ],
   imports: [
     BrowserModule,
-    MatToolbarModule,
-    MatButtonModule,
     BrowserAnimationsModule,
     MatSidenavModule,
-    MatListModule,
-    MatIconModule,
     RouterModule.forRoot(
       routes,
       { enableTracing: true } // <-- debugging purposes only
     ),
+
+    // For Navigation
+    MatToolbarModule,
+    MatButtonModule,
+    MatCardModule,
+    MatListModule,
+    MatIconModule,
+
+    // For RSVP Form
+    MatStepperModule,
+    FormsModule,
+    ReactiveFormsModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatCheckboxModule,
+    MatChipsModule,
   ],
-  providers: [],
+  providers: [
+    FormBuilder
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
