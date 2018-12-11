dde38ce80fc22511ec1c3c1624e6573c2cff052b
Michael <mikegeeraert@gmail.com>
2018-12-10 21:22:31 -0600

    remove unused code

--- src/app/rsvp-form/rsvp-form.component.ts
+++ src/app/rsvp-form/rsvp-form.component.ts
@@ -20,11 +20,6 @@ enum State {
 export class RsvpFormComponent implements OnInit {
   state: State = State.loading;
 
-  rsvpForm: FormGroup;
-  attendance: FormGroup;
-  plusOnes: FormGroup;
-  personalRequests: FormGroup;
-
   household: Household;
 
   constructor(private fb: FormBuilder,
@@ -33,7 +28,6 @@ export class RsvpFormComponent implements OnInit {
 
   ngOnInit() {
     this.storage.getHousehold('fleetwoodmac').subscribe((household: Household) => {
-      //   console.log(household);
       this.household = household;
       this.state = State.success;
       },
@@ -44,59 +38,6 @@ export class RsvpFormComponent implements OnInit {
           duration: 2000,
         });
       });
-  }
-
-  buildRSVPForm(household: Household): FormGroup {
-
-    // Build Attendance Controls
-    const attendanceControls = household.members.reduce((controls, member) => {
-      controls[member.id] = this.fb.control(member.isComing);
-      return controls;
-    }, {});
-
-    this.attendance = this.fb.group(attendanceControls);
-
-    // Build Plus One Controls
-    const plusOneControls = household.members.reduce((controlGroups, member) => {
-      if (member.allowedPlusOne) {
-        const memberPlusOneControls = this.fb.group({
-          isComing: this.fb.control({value: !!member.plusOne, disabled: member.isComing}),
-          plusOneFirst: this.fb.control(member.plusOne ? member.plusOne.first : ''),
-          plusOneLast: this.fb.control(member.plusOne ? member.plusOne.last : '')
-        });
-        console.log(memberPlusOneControls);
-        controlGroups[member.id] = memberPlusOneControls;
-      }
-      return controlGroups;
-    }, {});
-    this.plusOnes = this.fb.group(plusOneControls);
-
-    // Build Personal Requests controls
-    const personalRequestControls = household.members.reduce((controls, member) => {
-      controls[member.id] = this.fb.control(member.dietaryRestrictions);
-      return controls;
-    }, {
-      accommodation: this.fb.control(household.accommodation),
-      songs: this.fb.control(household.songs),
-      drinks: this.fb.control(household.drinks)
-    });
-    this.personalRequests = this.fb.group(personalRequestControls);
-
-    // Enable or Disable controls based on Attendance
-    for (const controlKey in attendanceControls) {
-      if (attendanceControls.hasOwnProperty(controlKey)) {
-        attendanceControls[controlKey].valueChanges.subscribe((isComing: boolean) => {
-          // Enable/Disable Plus One Control
-          if (plusOneControls.hasOwnProperty(controlKey)) {
-            // If not isComing, plus one option should be disabled
-            const control = plusOneControls[controlKey];
-            isComing ? control.enable() : control.disable();
-          }
-        });
-      }
-    }
-
-    return this.fb.group([this.attendance, this.plusOnes, this.personalRequests]);
   }
 
   togglePlusOne($event: MatCheckboxChange, member: Member) {
